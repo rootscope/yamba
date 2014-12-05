c97250f2250cd07a8857f1465e78157995f61e99
rootscope <datrootscope@gmail.com>
2014-12-04 21:41:20 -0700

    yamba7 init

---
+++ Yamba6/src/com/rootscope/yamba7/MainActivity.java
@@ -0,0 +1,46 @@
+package com.rootscope.yamba7;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+		case R.id.action_settings:
+			startActivity(new Intent(this, SettingsActivity.class));
+			return true;
+		case R.id.action_tweet:
+			startActivity(new Intent(this, StatusActivity.class));
+			return true;
+		case R.id.action_refresh:
+			startService(new Intent(this, RefreshService.class));
+			return true;
+		case R.id.action_purge:
+			int rows = getContentResolver().delete(StatusContract.CONTENT_URI, null, null);
+			Toast.makeText(this, "deleted "+rows+" rows", Toast.LENGTH_LONG).show();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+}
---
+++ Yamba6/src/com/rootscope/yamba7/StatusActivity.java
@@ -0,0 +1,20 @@
+package com.rootscope.yamba7;
+
+import android.os.Bundle;
+
+public class StatusActivity extends SubActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		if(savedInstanceState == null) {
+			StatusFragment fragment = new StatusFragment();
+			getFragmentManager()
+				.beginTransaction()
+				.add(android.R.id.content, fragment, fragment.getClass().getSimpleName()).commit();
+			System.out.println(fragment);
+		}
+		
+	}
+}
